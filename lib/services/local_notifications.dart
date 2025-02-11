11be9708cd5bf0c048c893b405d61f0e1f62c031
PrasadMusini-Dev <durgaprasad.musini@calibrage.in>
2025-02-11 11:27:09 +0530

    added googleservice-info.plist

--- lib/services/local_notifications.dart
+++ lib/services/local_notifications.dart
@@ -2,13 +2,10 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// import 'package:hairfixingservice/HomeScreen.dart';
+
 
 import '../notifications_screen.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotificationService {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
